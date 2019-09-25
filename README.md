db2858edbfac82a3ec5a7dc3bc51b943b4927a18
rikad <rikadscreamo@gmail.com>
2019-09-25 17:11:37 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nodechartjs
 belajar koneksi chartjs ke nodejs (Rest API sederhana)
+
+# Run
+npm install
+node index.js
+
+open browser http://localhost:3000
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//serving public folder
+app.use(express.static('public'))
+
+app.get('/api/data', function (req, res) { 
+    var data = {
+        line1 : Math.floor(Math.random() * 100),
+        line2 : Math.floor(Math.random() * 100)
+    }
+
+    res.send(data) 
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
